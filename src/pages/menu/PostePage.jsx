1bdcae8a29cd8544ea4870d380be196470c5b760
Isaias <isasalas145@gmail.com>
2024-01-17 15:45:07 -0400

    first commit

--- src/pages/menu/PostePage.jsx
+++ src/pages/menu/PostePage.jsx
@@ -12,36 +12,52 @@ import {
 const PostePage = () => {
   return (
     <Grid container>
-      <Card variant="outlined">
-        {" "}
-        <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            Registrados
-          </Typography>
-          <Typography
-            variant="h2"
-            fontWeight={"bold"}
-            color="text.secondary"
-            component="div"
+      <Grid item xs={12} md={4}>
+        <Card variant="outlined">
+          {" "}
+          <CardContent
+            style={{ display: "flex", flexDirection: "column", flex: 1 }}
           >
-            2145
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            Postes
-          </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-            <hr />
-          </Typography>
-          <CardActions style={{ padding: 0 }}>
-            <Button fullWidth startIcon={<Add />} variant="contained">
-              solucionar
-            </Button>
-            <Button fullWidth startIcon={<Add />} variant="contained">
-              añadir
-            </Button>
-          </CardActions>
-        </CardContent>
-      </Card>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              Registrados
+              <hr />
+            </Typography>
+
+            <Grid container flexDirection={"column"} margin={0} flex={1}>
+              <Typography
+                variant="h1"
+                fontWeight={"bold"}
+                color="text.secondary"
+                component="div"
+              >
+                2145
+              </Typography>
+              <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                postes
+              </Typography>
+            </Grid>
+            <Typography
+              sx={{ fontSize: 14 }}
+              color="text.secondary"
+              gutterBottom
+            >
+              <hr />
+            </Typography>
+            <CardActions style={{ padding: 0 }}>
+              <Button fullWidth startIcon={<Add />} variant="contained">
+                solucionar
+              </Button>
+              <Button fullWidth startIcon={<Add />} variant="contained">
+                añadir
+              </Button>
+            </CardActions>
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
   );
 };
