36aca69b22c4cd8602f409588051aee5655d4fae
Isaias <isasalas145@gmail.com>
2024-01-23 23:35:31 -0400

    first commit

--- src/pages/menu/EventoPage.jsx
+++ src/pages/menu/EventoPage.jsx
@@ -13,16 +13,30 @@ import {
 
 import errorPng from "../../assets/images/error.png";
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Autocomplete,
   Box,
   Button,
   ButtonGroup,
   Card,
   CardActions,
   CardContent,
+  Checkbox,
+  FormControlLabel,
   Grid,
+  TextField,
   Typography,
 } from "@mui/material";
-import { Add, Edit, EmojiObjects, WhereToVote } from "@mui/icons-material";
+import {
+  Add,
+  ArrowDownward,
+  ArrowDropDown,
+  Edit,
+  EmojiObjects,
+  WhereToVote,
+} from "@mui/icons-material";
 import {
   columnsData,
   markersData,
@@ -82,11 +96,23 @@ const EventoPage = () => {
   return (
     <Grid
       container
-      sx={{ height: { md: "calc(100% - 76px)" } }}
+      sx={{
+        height: { xs: "auto", md: "calc(100vh - 64px)" },
+      }}
+      m={0}
       alignItems={"stretch"}
       className="evento-page"
     >
-      <Grid display={"flex"} flexDirection={"column"} item xs={12} md={3}>
+      <Grid
+        item
+        sx={{
+          maxHeight: { xs: "auto", md: "100%" },
+        }}
+        display={"flex"}
+        flexDirection={"column"}
+        xs={12}
+        md={3}
+      >
         <Card
           style={{
             flex: 1,
@@ -145,28 +171,264 @@ const EventoPage = () => {
               </ButtonGroup>
             </CardActions>
 
-            <Card
-              style={{ flex: 1, display: "flex", flexDirection: "column" }}
+            <Grid
+              component={Card}
+              sx={{
+                padding: 0,
+                borderRadius: "4px",
+                flex: 1,
+                display: "flex",
+                flexDirection: "column",
+                maxHeight: {
+                  xs: "100%",
+                  md: "calc(100vh - 170px)",
+                },
+                overflow: "auto",
+              }}
               variant="outlined"
             >
               <CardContent
-                style={{ display: "flex", flexDirection: "column", flex: 1 }}
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  flex: 1,
+                  padding: "6px",
+                }}
               >
-                <Grid container flexDirection={"column"} margin={0} flex={1}>
-                  <Typography
-                    variant="h5"
-                    fontWeight={"bold"}
-                    color="text.secondary"
-                    component="div"
+                <Grid
+                  container
+                  p={0}
+                  m={0}
+                  style={{ justifyContent: "stretch" }}
+                  noValidate
+                  autoComplete="off"
+                >
+                  {" "}
+                  <Grid item xs={12} md={5}>
+                    <Autocomplete
+                      size="small"
+                      style={{
+                        padding: 0,
+                        margin: 0,
+                      }}
+                      options={[
+                        { label: "123", year: 1994 },
+                        { label: "323", year: 1972 },
+                      ]}
+                      renderInput={(params) => (
+                        <TextField
+                          required
+                          style={{
+                            padding: 0,
+                            margin: 0,
+                            width: "100%",
+                          }}
+                          fullWidth
+                          variant="outlined"
+                          {...params}
+                          label="Poste"
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid style={{}} item xs={12} md={7}>
+                    <TextField
+                      fullWidth
+                      style={{
+                        width: "100%",
+                        padding: 0,
+                        margin: 0,
+                      }}
+                      variant="outlined"
+                      multiline
+                      size="small"
+                      label="Comentarios"
+                    />
+                  </Grid>
+                  <Grid
+                    component={Card}
+                    variant="outlined"
+                    sx={{ margin: { xs: "2px", sm: "4px" }, padding: 2 }}
+                    item
+                    xs={12}
                   >
-                    Detalle
-                  </Typography>
-                  <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    poste
-                  </Typography>
+                    <Typography
+                      display={"flex"}
+                      color="text.secondary"
+                      variant="p"
+                      textAlign={"left"}
+                    >
+                      Imagen:
+                    </Typography>
+                    <img
+                      width={"100%"}
+                      style={{
+                        aspectRatio: "1/1",
+                        objectFit: "cover",
+                        borderRadius: 4,
+                      }}
+                      src={`https://i.pinimg.com/564x/d2/d3/a9/d2d3a9174b19ddd1dad2603e142b058c.jpg`}
+                      alt={"imagen"}
+                      loading="lazy"
+                    />
+                  </Grid>
+                  <Grid style={{}} item xs={12}>
+                    <Typography textAlign={"left"} p={1}>
+                      Observaciones:
+                    </Typography>
+
+                    <Accordion sx={{}} variant="outlined">
+                      <AccordionSummary expandIcon={<ArrowDropDown />}>
+                        <Typography>Fibra optica</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          textAlign: "left",
+                          padding: "4px",
+                          margin: "0px",
+                        }}
+                      >
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            width: "100%",
+                            margin: "0px",
+                          }}
+                          label="Poste haciendo fricción en fibra"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                      </AccordionDetails>
+                    </Accordion>
+                    <Accordion sx={{}} variant="outlined">
+                      <AccordionSummary expandIcon={<ArrowDropDown />}>
+                        <Typography>Ferrreteria</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          textAlign: "left",
+                          padding: "4px",
+                          margin: "0px",
+                        }}
+                      >
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            width: "100%",
+                            margin: "0px",
+                          }}
+                          label="Poste haciendo fricción en fibra"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                      </AccordionDetails>
+                    </Accordion>
+                    <Accordion sx={{}} variant="outlined">
+                      <AccordionSummary expandIcon={<ArrowDropDown />}>
+                        <Typography>Poste</Typography>
+                      </AccordionSummary>
+                      <AccordionDetails
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          textAlign: "left",
+                          padding: "4px",
+                          margin: "0px",
+                        }}
+                      >
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            width: "100%",
+                            margin: "0px",
+                          }}
+                          label="Poste haciendo fricción en fibra"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                      </AccordionDetails>
+                    </Accordion>
+                    <Accordion sx={{}} variant="outlined">
+                      <AccordionSummary expandIcon={<ArrowDropDown />}>
+                        <Typography>Terreno </Typography>
+                      </AccordionSummary>
+                      <AccordionDetails
+                        sx={{
+                          display: "flex",
+                          flexDirection: "column",
+                          textAlign: "left",
+                          padding: "4px",
+                          margin: "0px",
+                        }}
+                      >
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            width: "100%",
+                            margin: "0px",
+                          }}
+                          label="Poste haciendo fricción en fibra"
+                        />
+                        <FormControlLabel
+                          control={<Checkbox />}
+                          sx={{
+                            margin: "0px",
+                          }}
+                          label="Observación"
+                        />
+                      </AccordionDetails>
+                    </Accordion>
+                  </Grid>
                 </Grid>
               </CardContent>
-            </Card>
+            </Grid>
           </CardContent>
         </Card>
       </Grid>
@@ -174,10 +436,22 @@ const EventoPage = () => {
       <Grid display={"flex"} flexDirection={"column"} item xs={12} md={9}>
         <Card sx={{ flex: 1 }} variant="outlined" style={{}}>
           <CardContent style={{}}>
-            <Box sx={{ height: "calc(100vh - 145px)" }}>
+            <Box
+              sx={{
+                height: {
+                  xs: "calc(100vh - 105px)",
+                  md: "calc(100vh - 122px)",
+                },
+                width: {
+                  xs: "calc(100vw - 100px)",
+                  sm: "calc(100vw - 115px)",
+                  md: "auto",
+                },
+              }}
+            >
               <DataGrid
+                //className="datagrid-content"
                 {...data}
-                className="datagrid-content"
                 experimentalFeatures={{ lazyLoading: true }}
                 rowsLoadingMode="server"
                 rowCount
